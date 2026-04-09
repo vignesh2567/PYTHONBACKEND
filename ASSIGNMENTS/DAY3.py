417f0fbc903db5ca2bcf5380f56b2d86e8a3cc36
vignesh2526 <vigneshb2567@gmail.com>
2026-04-09 16:40:15 +0530

    DONE

---
+++ ASSIGNMENTS/DAY3.py
@@ -0,0 +1,29 @@
+
+#LADDER IF ELSE
+
+point = int(input ("ENTER YOUR POINT :"))
+
+if point > 20:
+    print("Enter a valid point")
+elif point == 20:
+    print ("QUALIFIED")
+elif point < 20 and point >= 14:
+    print("Can get QUALIFIED as a 2nd,3rd,4th TEAM")
+elif point <14 and point >= 10:
+    print("HARD to QUALIFY")
+else:
+    print("YOU CAN'T QUALIF THIS SEASON")
+
+
+# NESTED IF ELSE
+
+spec1 = int(input("Enter your RAM size :"))
+spec2 = input("INTEL 5  OR AMD INTEL 7")
+
+if spec1 >= 16:
+    if spec2 == "INTEL 7":
+        print("YOU CAN DOWNLOAD THE GAME")
+    else:
+        print("Not having required processor")
+else:
+    print ("the game is not your device")

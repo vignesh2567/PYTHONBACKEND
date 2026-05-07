77abad221faf7ae8a97521a6b40fe8798059d4a9
vignesh2526 <vigneshb2567@gmail.com>
2026-05-07 12:56:07 +0530

    DONE

---
+++ ASSIGNMENTS/DAY19.py
@@ -0,0 +1,17 @@
+
+#SORTING WITHOUT BUILT IN FUNCTION
+
+# SORTING WITHOUT USING BUILT-IN FUNCTION
+
+numbers = [5, 2, 8, 1, 9,7,11,0]
+
+# Bubble Sort
+for i in range(len(numbers)):
+    for j in range(len(numbers) - 1 - i):
+        if numbers[j] > numbers[j + 1]:
+            # swap
+            temp = numbers[j]
+            numbers[j] = numbers[j + 1]
+            numbers[j + 1] = temp
+
+print("Sorted List:", numbers)
